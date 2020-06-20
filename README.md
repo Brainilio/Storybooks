b0a552d707d91f44ebea5bef8e0e1ac2244e22e6
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-06-20 22:02:15 +0200

    Set up environment and connected to MongoDB

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Storybooks
- Node.js app using express, mongoDB & google oauth
+
+Node.js app using express, mongoDB & google oauth
+
+Entry point:
+app.js
+
+To start app (production):
+"npm start"
+
+To start app (development):
+"npm run dev"
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+
+// Load config file
+dotenv.config({ path: "./config/config.env" })
+
+// connect db
+
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
